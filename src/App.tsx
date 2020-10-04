341af655c1a1b43c66ec98ccac382c13bb831901
omri wallach <omriwallach1992@gmail.com>
2020-10-04 16:42:27 +0200

    add User and books component to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React,{useState} from 'react';
+import {User,Book} from './components/index'
+import { IUserProps } from './components/user/User';
+import { IBook } from './types';
 
 function App() {
+  const [books, setBooks] = useState<IBook[]>([{
+    bookTitle: "harry potter",
+    id: 0,
+    numberOfCopies: 2,
+    numberOfOrigin: 1
+  },
+  {
+    bookTitle: "The hobbit",
+    id: 1,
+    numberOfCopies: 3,
+    numberOfOrigin: 4
+    }])
+  const [userBook, setUserBook] = useState<IUserProps>({books:[]})
+  
   return (
-<>
-      APP
+    <>
+      <User {...userBook} />
+      <h3>Library books</h3>
+      <div>
+      {
+        books.map((book, index) => <Book key={`book-${index}`} {...book}/>)
+      }
+      </div>
       </>
   );
 }
---
+++ src/components/index.ts
@@ -0,0 +1,2 @@
+export { Book } from './book/Book'
+export {User} from './user/User'
