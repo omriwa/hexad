0a36528939f98a9771a43475f09779ddb0d69449
omri wallach <omriwallach1992@gmail.com>
2020-10-05 19:48:27 +0200

    test user return function

--- src/components/user/User.test.tsx
+++ src/components/user/User.test.tsx
@@ -1,26 +1,47 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import {User,IUserProps} from './User'
+import { render ,fireEvent} from '@testing-library/react';
+import { User, IUserProps } from './User'
 
-test('renders book card with props', () => {
-    const bookProp: IUserProps = {
+var bookProp: IUserProps;
+
+beforeEach(() => {
+    bookProp = {
         books: [
             {
                 bookTitle: "title1",
         id: 1,
         numberOfCopies: 1,
         numberOfOrigin: 2
-            },
-            {
-                bookTitle: "title2",
-        id: 1,
-        numberOfCopies: 3,
-        numberOfOrigin: 4
             }
-        ]
+        ],
+        returnCopyBook: jest.fn(),
+        returnOriginalBook: jest.fn()
     }
+})
+
+test('renders book card with props', () => { 
     const { container } = render(<User {...bookProp} />);
     const listItems = container.querySelectorAll('li')
 
     expect(listItems.length).toBe(bookProp.books.length * 2);
+});
+
+test('should click on return original book', () => {
+    const { getByText } = render(<User {...bookProp} />);
+    const returnOriginalBookButton = getByText('Return orignal book');
+    jest.spyOn(bookProp, 'returnOriginalBook').mockImplementation(jest.fn);
+    fireEvent.click(returnOriginalBookButton)
+
+    expect(returnOriginalBookButton).toBeInTheDocument();
+    expect(bookProp.returnOriginalBook).toBeCalled();
+});
+
+test('should click on return copy book', () => {
+    const { getByText } = render(<User {...bookProp} />);
+    const returnCopyBookButton = getByText('Return copy book');
+    jest.spyOn(bookProp, 'returnCopyBook').mockImplementation(jest.fn);
+    fireEvent.click(returnCopyBookButton)
+
+    expect(returnCopyBookButton).toBeInTheDocument();
+    expect(bookProp.returnCopyBook).toBeCalled();
 });
